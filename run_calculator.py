de270cc6adef2682f67481f7e968ea7260a2225a
hermannmichel <61755101+hermannmichel@users.noreply.github.com>
2020-03-04 21:54:53 +0100

    new function

--- run_calculator.py
+++ run_calculator.py
@@ -1,3 +1,11 @@
 # skript, welches nur ausgeführt werden muss, um den TR zu starten
 # ich ändere jetzt hier etwas in work weil ich rumspiele, möchte aber nicht, dass es in "master" drin ist.
 # alles was ich in master hochlade ist quasi schon eine Teil Abgabe. im Work Branch kann ich so viel Mist machen wie ich möchte
+# %%
+test = r'c:\Users\Hermann\VS_Projects\dummy.py'
+print(test)
+
+'r'
+
+for i in range(4):
+    print(i)
