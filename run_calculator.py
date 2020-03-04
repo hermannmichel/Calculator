e96c546499a4fb798b7dd46bcb5923a1b3c3349c
hermannmichel <61755101+hermannmichel@users.noreply.github.com>
2020-03-04 21:22:38 +0100

    test änderungen

--- run_calculator.py
+++ run_calculator.py
@@ -1,1 +1,3 @@
 # skript, welches nur ausgeführt werden muss, um den TR zu starten
+# ich ändere jetzt hier etwas in work weil ich rumspiele, möchte aber nicht, dass es in "master" drin ist.
+# alles was ich in master hochlade ist quasi schon eine Teil Abgabe. im Work Branch kann ich so viel Mist machen wie ich möchte
